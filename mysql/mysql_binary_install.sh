98762df3fad9c5934c60992dd9e23cd57bea3d9d
JialeHao <haovco@163.com>
2022-12-21 22:24:33 +0800

    feat: add mysql install script

---
+++ mysql/mysql_binary_install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function usage () {
+    echo "Usage:"
+    echo "$0 -f file -i conf\n"
+    echo "-f file: mysql binary package"
+    echo "-i conf: mysql configuration file"
+    exit 1
+}
+
+while getopts ':f:i:' OPT; do
+   case "${OPT}" in
+       f) INSTALL_PACKAGE=${OPTARG};;
+       i) CONFIG_FILE=${OPTARG};;
+       *) usage;;
+   esac
+done
+
+BASE_DIR="/usr/local/mysql"
+DATA_BASE_DIR="/opt/mysql"
+DATA_DIR="${DATA_BASE_DIR}/data"
+RUN_DIR="${DATA_BASE_DIR}/run"
+LOG_DIR="${DATA_BASE_DIR}/log"
+
+
+groupadd -r mysql
+useradd -r -g mysql -s /bin/false mysql
+
